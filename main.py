c3ff0eb3866d4535af15ff6aece7b08517538a81
RuslanMuzichenko <russia.muzichenko@gmail.com>
2021-02-21 21:19:17 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,75 +1,76 @@
-import urllib3
+import sys
+import time
 import xml.etree.ElementTree as XML
 import requests
-import time
 from colorama import Fore
 
-class HuaweiLTE(object):
 
-    def __init__(self):
-        host = 'http://192.168.8.1/'
+def ip_checker() -> str:
+    try:
+        ip = requests.get('https://api.ipify.org', timeout=30)
+        if ip.status_code == 200:
+            return ip.text
+        else:
+            print('SERVER NOT RESPONDING: https://api.ipify.org')
+            sys.exit()
+    except requests.exceptions.ConnectionError:
+        print('NO CONNECTION')
+
+
+def session(host: str) -> dict:
+    response = requests.get(f'http://{host}/api/webserver/SesTokInfo')
+    root = XML.ElementTree(XML.fromstring(response.content)).getroot()
+    headers = {'__RequestVerificationToken': f'{root[1].text}',
+               '_ResponseSource': 'Broswer',
+               'Cookie': f'SessionID={root[0].text}'}
+    return headers
+
+
+def net_mode(host: str) -> str:
+    response = requests.get(f'http://{host}/api/net/net-mode')
+    root = XML.ElementTree(XML.fromstring(response.content)).getroot()
+    net = root[0].text
+    return net
+
+
+def device_name(host: str) -> str:
+    response = requests.get(f'http://{host}/api/device/information')
+    root = XML.ElementTree(XML.fromstring(response.content)).getroot()
+    name = root[0].text
+    return name
+
+
+def ip_maker(host: str, net_send: str) -> None:
+    headers = session(host)
+    payload = f"<?xml version='1.0' encoding='UTF-8'?><request><NetworkMode>{net_send}" \
+              f"</NetworkMode><NetworkBand>3FFFFFFF</NetworkBand><LTEBand>800C5</LTEBand></request>"
+    requests.post(f'http://{host}/api/net/net-mode', data=payload, headers=headers)
+
+
+class Modem:
+
+    def __init__(self, host: str = '192.168.8.1', lte=False):
+        self.mode = lte
         self.host = host
-        ctrl_time = time.time()
-        self.ctrl_time = ctrl_time
-        try:
-            ip = requests.get('https://ipinfo.io', timeout=30).json()
-            old_ip =  ip['ip']
-            self.old_ip = old_ip
-            self.session()
-        except:
-            self.session()
-            self.reboot()
+        self.time_control = time.time()
+        self.old_ip = ip_checker()
+        self.make_ip()
 
-    def session(self):
-        http = urllib3.PoolManager()
-        self.http = http
-        response_body = http.request('GET', f'{self.host}api/webserver/SesTokInfo')
-        tree = XML.ElementTree(XML.fromstring(response_body.data))
-        root = tree.getroot()
-        session = root[0].text
-        tokken = root[1].text
-        self.session = session
-        self.tokken = tokken
-        response_body = http.request('GET', f'{self.host}api/device/information')
-        tree = XML.ElementTree(XML.fromstring(response_body.data))
-        root = tree.getroot()
-        modem = root[0].text
-        self.modem = modem
-
-
-    def reboot(self):
-        headers = {'__RequestVerificationToken': f'{self.tokken}',
-                   '_ResponseSource': 'Broswer',
-                   'Cookie': f'SessionID={self.session}'
-                   }
-        data = "<?xml version='1.0' encoding='UTF-8'?><request><Control>1</Control></request>"
-        result = self.http.request('POST', f'{self.host}api/device/control', body=data, headers=headers)
-        if 'OK' in result.data.decode():
-            print(f'{self.modem} перезагружен')
-
-
-    def ip_make(self):
-        response_body = self.http.request('GET', f'{self.host}api/net/net-mode')
-        tree = XML.ElementTree(XML.fromstring(response_body.data))
-        root = tree.getroot()
-        Net = root[0].text
-        if Net == '02':
-            Net_send = '03'
+    def make_ip(self):
+        _net = net_mode(self.host)
+        if _net == '02':
+            ip_maker(self.host, net_send='03')
+        elif self.mode:
+            ip_maker(self.host, net_send='02')
+            ip_maker(self.host, net_send='03')
         else:
-            Net_send = '02'
-        headers = {'__RequestVerificationToken': f'{self.tokken}',
-                   '_ResponseSource': 'Broswer',
-                   'Cookie': f'SessionID={self.session}'
-                   }
-        data = f"<?xml version='1.0' encoding='UTF-8'?><request><NetworkMode>{Net_send}</NetworkMode><NetworkBand>3FFFFFFF</NetworkBand><LTEBand>800C5</LTEBand></request>"
-        result = self.http.request('POST', f'{self.host}api/net/net-mode', body=data, headers=headers)
-        if 'OK' in result.data.decode():
-            ip = requests.get('https://ipinfo.io', timeout=30).json()
-            new_ip = ip['ip']
-            if self.old_ip != new_ip:
-                cure_time = round(time.time() - self.ctrl_time)
-                print(Fore.BLUE + f'{self.modem}: получен IP: {new_ip } за {cure_time}sec')
+            ip_maker(self.host, net_send='02')
+        new_ip = ip_checker()
+        if self.old_ip != new_ip:
+            print(Fore.BLUE +
+                  f'{device_name(self.host)}: {new_ip} time: {round(time.time() - self.time_control)}sec')
         else:
-            HuaweiLTE().ip_make()
+            self.make_ip()
 
-HuaweiLTE().ip_make()
+
+#Modem(lte=True)
